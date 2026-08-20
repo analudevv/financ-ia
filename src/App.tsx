f08bdead21bbce51848b1dc3f75ddad0ec960711
analudevv <devanaluizalima@gmail.com>
2026-08-20 16:47:36 -0300

    feat: implementacao do Header da pagina e mudança de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 function App() {
-  return (
-    <>
-      <h1>Financia</h1>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
