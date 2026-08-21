a99a5d493634916984475a87175d7192d88e1641
analudevv <devanaluizalima@gmail.com>
2026-08-21 18:49:35 -0300

    feat: implementação do formulario e navegação

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,17 +1,53 @@
-import { PiggyBank } from "lucide-react";
+import { simulationFormSteps, type SimulationFormData } from "@/data/simulation";
 import { FormStep } from "./FormStep";
 import { StepProgress } from "./Progress";
+import { useState } from "react";
+import { useSimulationStorage } from "@/hooks/useSimulationStorage";
+import { useNavigate } from "react-router-dom";
 
-export function SimulationForm() {
+export const SimulationForm = () => {
+	const { saveFormData } = useSimulationStorage()
+	const [formData, setFormData] = useState<SimulationFormData>(
+		{} as SimulationFormData
+	)
+	const [currentStepIndex, setCurrentStepIndex] = useState(0);
+	const totalSteps = simulationFormSteps.length;
+	const currentStep = simulationFormSteps[currentStepIndex];
+
+	const navigate = useNavigate()
+
+	const handleNextStep = (value: string) => {
+		const updateFormData = { ...formData, [currentStep.id]: value }
+		setFormData(updateFormData)
+
+
+		if (currentStepIndex + 1 > totalSteps - 1) {
+			saveFormData(updateFormData)
+			void navigate('/result')
+			return;
+		}
+
+		setCurrentStepIndex((prev) => prev + 1);
+	};
+
+	const handlePreviousStep = () => {
+		if (currentStepIndex === 0) {
+			return;
+		}
+		setCurrentStepIndex((prev) => prev - 1);
+	};
+
 	return (
 		<>
-
-			<StepProgress currentStep={0} totalSteps={5} />
+			<StepProgress currentStep={currentStepIndex + 1} totalSteps={totalSteps} />
 			<FormStep
-				icon={PiggyBank}
-				title="Renda Mensal Bruta"
-				question="Quanto é depositado na sua conta todo mês? (Salário, renda extra, etc.)"
+				key={currentStep.id}
+				{...currentStep}
+				onBack={handlePreviousStep}
+				onNext={handleNextStep}
+				hideBackButton={currentStepIndex === 0}
 			/>
 		</>
 	)
 }
+
--- src/components/features/Simulation/Hero.tsx
+++ src/components/features/Simulation/Hero.tsx
@@ -2,7 +2,7 @@ import PiggyBankImage from '@/assets/images/piggy-bank.png'
 
 export function SimulationHero() {
 	return (
-		<div className="mb-8 text-center mt-50">
+		<div className="mb-8 text-center mt-5">
 			<div className="flex flex-col items-center sm:flex-row">
 				<h1 className="text-foreground text-3xl font-semibold sm:text-4xl">
 					Vamos planejar seu futuro
